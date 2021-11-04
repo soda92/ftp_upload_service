21d97c966a8e88e1f00786f4a1c67083f395bb90
SodaCris <18463922396@163.com>
2021-11-05 07:59:43 +0800

    add dir listing

--- upload.py
+++ upload.py
@@ -1,6 +1,23 @@
 import config
 from ftplib import FTP
+
+# l = []
+
+
+def f(arr: list):
+    def g(line):
+        global s
+        arr.append(line)
+    return g
+
+
 with FTP() as ftp:
-    ftp.connect(host=config.server_addr,port=config.server_port)
+    # ftp.debug(level=2)
+    ftp.connect(host=config.server_addr, port=config.server_port)
     ftp.login(user=config.username, passwd=config.password)
-    print(ftp.retrlines("LIST"))
+    arr = []
+    ftp.retrlines("LIST", callback=f(arr))
+    print(arr)
+
+# size_unused = len("drwxrwxr-x    6 1000     1000         4096 Nov 03 08:01 ")
+# print(l[0][size_unused:])

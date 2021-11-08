15c2bec30c25ee6589e3985768365d4ad288652a
sodacris <glorykings@foxmail.com>
2021-11-08 10:13:14 +0800

    remove comments

--- upload.py
+++ upload.py
@@ -15,54 +15,11 @@ def f(arr: list):
     return g
 
 
-# def is_dir_exist(ftp: FTP, dirname: Path):
-#     # size_unused = len(
-#     #     "drwxrwxr-x    6 1000     1000         4096 Nov 03 08:01 ")
-#     size_unused = 56
-#     arr = []
-#     ftp.retrlines(cmd="LIST", callback=f(arr))
-#     names = [line[size_unused-1:] for line in arr]
-#     types = [line[0] for line in arr]
-#     for i in range(len(names)):
-#         if types[i] == 'd':
-#             if names[i] == dirname.name:
-#                 return True
-#     return False
-
-# def get_filenames(ftp: FTP, dir_name: Path):
-#     # size_unused = len(
-#     #     "drwxrwxr-x    6 1000     1000         4096 Nov 03 08:01 ")
-#     ftp.cwd(str(dir_name))
-#     size_unused = 56
-#     arr = []
-#     ftp.retrlines(cmd="LIST", callback=f(arr))
-#     names = [line[size_unused-1:] for line in arr]
-#     types = [line[0] for line in arr]
-#     file_names = []
-#     for i in range(len(names)):
-#         if types[i] == '-':
-#             file_names.append(names[i])
-#     return file_names
-
-
 def transfer_file(srcfile: Path, destfile: Path, ftp: FTP):
     ftp.cwd(str(destfile.parent))
     logging.info(f"transfering file: {str(destfile)}")
     with open(str(srcfile), mode='rb') as fp:
         ftp.storbinary(cmd=f"STOR {srcfile.name}", fp=fp)
-
-
-# def size_equal(srcfile: Path, destfile: Path, ftp: FTP):
-#     ftp.cwd(str(destfile.parent))
-#     size = 0
-#     try:
-#         ftp.retr
-#         size = ftp.size(destfile.name)
-#     except:
-#         return False
-#     finally:
-#         src_file_size = os.path.getsize(str(srcfile))
-#         return size == src_file_size
 
 
 def transfer_folder(srcfolder: Path, destfolder: Path, ftp: FTP):
